70b96fce0ec224b0f7292411d9c23b7e1ec54f88
Akhalil123321 <ahmed.mohamed.khalil1998@gmail.com>
2022-08-02 23:21:13 +0200

    add cart ditailes page

--- src/components/Detailes.js
+++ src/components/Detailes.js
@@ -2,6 +2,23 @@
 import React from 'react';
 import FavoritesData from '../data/favoritesData'
 export default function Cards(props){
+    
+    function img1Clicked(){
+        let largeImg = document.querySelector('.card-dit-img-large')
+        largeImg.src=`./images/products/${props.img}`
+    }
+    function img2Clicked(){
+        let largeImg = document.querySelector('.card-dit-img-large')
+        largeImg.src=`./images/products/${props.imgdet1}`
+    }
+    function img3Clicked(){
+        let largeImg = document.querySelector('.card-dit-img-large')
+        largeImg.src=`./images/products/${props.imgdet2}`
+    }
+    function img4Clicked(){
+        let largeImg = document.querySelector('.card-dit-img-large')
+        largeImg.src=`./images/products/${props.imgdet3}`
+    }
     const [favorite, setFavorite] = React.useState({
         isFavorite: false
     })
@@ -17,19 +34,34 @@ export default function Cards(props){
         const favoritesData = FavoritesData
         favoritesData.push(props)
     }
+    
 return(
     
-        <div className='cart-cards'>
-                <img src={`./images/products/${props.img}`} className='cart-prop'/>
-                <div className='card-ditailes'>
-                <div className='cart-state'>
-                    <h4 className='cart-content-name'>{props.name}</h4>
-                    <img src={`./images/${heartIcon}`} className='cart-heart' onClick={toggleFavorite}/>
+        <div className='card-dit-container'>
+            <div className='card-dit-img-container'>
+                <div className='card-dit-img-small'>
+                    <img src={`./images/products/${props.img}`} className='card-dit-img' onClick={img1Clicked}/>
+                    <img src={`./images/products/${props.imgdet1}`} className='card-dit-img' onClick={img2Clicked}/>
+                    <img src={`./images/products/${props.imgdet2}`} className='card-dit-img' onClick={img3Clicked}/>
+                    <img src={`./images/products/${props.imgdet3}`} className='card-dit-img' onClick={img4Clicked}/>
                 </div>
-                <p className='cart-discription'>{props.discription}</p>
-                    <h5 className='car-old-price'>{props.oldPrise} $</h5>
-                    <h5 className='cart-current-price'>{props.curruntPrise}<sup>{props.cents}</sup> $</h5>
+                <div className='card-dit-img-large-cont'>
+                    <img src={`./images/products/${props.imgdet3}`} className='card-dit-img-large'/>
                 </div>
             </div>
+            <div className='card-dit-information-cont'>
+                <div className='card-dit-state'>
+                    <h4 className='card-dit-name'>{props.name}</h4>
+                    <img src={`./images/${heartIcon}`} className='card-dit-heart' onClick={toggleFavorite}/>
+                </div>
+                    <p className='card-dit-discription'>{props.discription}</p>
+                <div className='card-price-cont'>
+                    <h5 className='card-dit-old-price'>{props.oldPrise} $</h5>
+                    <h5 className='card-dit-current-price'>{props.curruntPrise}<sup>{props.cents}</sup> $</h5>
+                </div>
+                <button className='card-dit-add-to-cart'>Add to cart</button>
+            </div>
+        </div>
+            
 )
 }
